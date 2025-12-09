fd13d319d25e8c7f68a3f171da00eba074f7f2a9
Zdravko <alexandrogarnacho94@gmail.com>
2025-12-09 19:11:41 +0200

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,1 +1,24 @@
-# main pipeline here
+from extract import extract_weather
+from transform import transform_weather
+from load import load_to_postgres
+
+def main():
+    print("Starting ETL pipeline...")
+
+    #  EXTRACT
+    print("Extracting weather data...")
+    raw_data = extract_weather()
+
+    #  TRANSFORM
+    print("Transforming data...")
+    df = transform_weather(raw_data)
+
+    #  LOAD
+    print("Loading data into PostgreSQL...")
+    load_to_postgres(df)
+
+    print("ETL pipeline completed successfully!")
+
+if __name__ == "__main__":
+    main()
+
